17205908ac05bb3f6082127235c8fee1a2190632
Jesus Herrera <jesusherrera1594@gmail.com>
2026-03-18 16:59:09 -0600

    Implement basic testing

--- starter/game_engine/src/lib.rs
+++ starter/game_engine/src/lib.rs
@@ -14,7 +14,18 @@ mod tests {
         // Create a game window
         ffi::safe_create_game_window("Test Window", 800, 600);
 
-        let sprite = ffi::safe_create_sprite(100.0, 100.0, 50, 50, 255, 0, 0);
+        while !ffi::safe_window_should_close() {
+            ffi::safe_clear_screen();
+            ffi::safe_update_game_window();
+            std::thread::sleep(std::time::Duration::from_millis(16));
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn test_sprite_rendering() {
+        ffi::safe_create_game_window("Sprite Test", 400, 400);
+        let sprite = ffi::safe_create_sprite(150.0, 150.0, 100, 100, 0, 255, 0);
 
         while !ffi::safe_window_should_close() {
             ffi::safe_clear_screen();
@@ -23,4 +34,65 @@ mod tests {
             std::thread::sleep(std::time::Duration::from_millis(16));
         }
     }
+
+    #[ignore]
+    #[test]
+    fn test_screen_clearing() {
+        ffi::safe_create_game_window("Clear Test", 400, 400);
+        let red_sprite = ffi::safe_create_sprite(150.0, 150.0, 100, 100, 255, 0, 0);
+        let green_sprite = ffi::safe_create_sprite(200.0, 200.0, 100, 100, 0, 255, 0);
+        let mut should_render_red_sprite = true;
+        while !ffi::safe_window_should_close() {
+            ffi::safe_clear_screen();
+            if should_render_red_sprite {
+                ffi::safe_render_sprite(red_sprite);
+                should_render_red_sprite = false;
+                ffi::safe_update_game_window();
+                std::thread::sleep(std::time::Duration::from_secs(5));
+            } else {
+                ffi::safe_render_sprite(green_sprite);
+                ffi::safe_update_game_window();
+                std::thread::sleep(std::time::Duration::from_millis(16));
+            }
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn test_key_presses() {
+        ffi::safe_create_game_window("Key Press Test", 400, 400);
+        let window = ffi::safe_get_window();
+        let mut up_key_pressed = false;
+        let mut down_key_pressed = false;
+        while ! up_key_pressed || ! down_key_pressed {
+             if ffi::safe_get_key(window, ffi::GLFW_KEY_UP) == 1 { // Up key
+                println!("Up key pressed!");
+                up_key_pressed = true;
+            }
+            if ffi::safe_get_key(window, ffi::GLFW_KEY_DOWN) == 1 { // Down key
+                println!("Down key pressed!");
+                down_key_pressed = true;
+            }
+            ffi::safe_clear_screen();
+            ffi::safe_update_game_window();
+            std::thread::sleep(std::time::Duration::from_millis(16));
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn test_sprite_position_update() {
+        ffi::safe_create_game_window("Sprite Position Update Test", 400, 400);
+        let sprite = ffi::safe_create_sprite(50.0, 50.0, 50, 50, 255, 0, 0);
+        let mut x = 50.0;
+        while !ffi::safe_window_should_close() {
+            ffi::safe_clear_screen();
+            ffi::safe_render_sprite(sprite);
+            ffi::safe_update_game_window();
+            x += 1.0;
+            ffi::safe_update_sprite_position(sprite, x, 50.0);
+            std::thread::sleep(std::time::Duration::from_millis(16));
+        }
+    }
+
 }
--- starter/game_engine/src/main.rs
+++ starter/game_engine/src/main.rs
@@ -1,11 +1,14 @@
 
 fn main() {
-    game_engine::ffi::safe_create_game_window("Test Window", 800, 600);
-    let sprite = game_engine::ffi::safe_create_sprite(100.0, 100.0, 50, 50, 255, 0, 0);
-    while !game_engine::ffi::safe_window_should_close() {
-        game_engine::ffi::safe_clear_screen();
-        game_engine::ffi::safe_render_sprite(sprite);
-        game_engine::ffi::safe_update_game_window();
-        std::thread::sleep(std::time::Duration::from_millis(16));
-    }
+        game_engine::ffi::safe_create_game_window("Sprite Position Update Test", 400, 400);
+        let sprite = game_engine::ffi::safe_create_sprite(50.0, 50.0, 50, 50, 255, 0, 0);
+        let mut x = 50.0;
+        while !game_engine::ffi::safe_window_should_close() {
+            game_engine::ffi::safe_clear_screen();
+            game_engine::ffi::safe_render_sprite(sprite);
+            game_engine::ffi::safe_update_game_window();
+            x += 1.0;
+            game_engine::ffi::safe_update_sprite_position(sprite, x, 50.0);
+            std::thread::sleep(std::time::Duration::from_millis(16));
+        }
 }
